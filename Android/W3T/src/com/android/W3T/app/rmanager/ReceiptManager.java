8672616167762f8a402932bc83df2779716adbe3
Yichao Yu <yichao@Masxaro.com>
2011-06-10 23:38:41 +0800

    add comments

--- Android/W3T/src/com/android/W3T/app/rmanager/ReceiptManager.java
+++ Android/W3T/src/com/android/W3T/app/rmanager/ReceiptManager.java
@@ -1,3 +1,31 @@
+/*
+ * ReceiptManager.java -- Receipt management class 
+ *
+ *  Copyright 2011 World Three Technologies, Inc. 
+ *  All Rights Reserved.
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Written by Yichao Yu <yichao@Masxaro>
+ * 
+ *  This class is used to manage the receipts: add/upload/keep the database 
+ *  on cellphone small.
+ *  Right now, I just use a fake receipt system and initialize the manager with
+ *  3 receipts.
+ *  
+ */
 package com.android.W3T.app.rmanager;
 
 import java.util.ArrayList;
@@ -37,7 +65,6 @@ public class ReceiptManager {
 		}
 			
 	}
-	
 	
 	
 	public static int getNumValid() {
