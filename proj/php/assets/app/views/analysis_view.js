ba342bb5289e99db56ebad6c29175fbe276fe24f
Jimmy Chao <daizenga@gmail.com>
2011-08-22 00:21:23 -0400

    [feature]add canvas to analysis page

--- proj/php/assets/app/views/analysis_view.js
+++ proj/php/assets/app/views/analysis_view.js
@@ -1,1 +1,25 @@
-window.AnalysisView = Backbone.View.extend({});
+window.AnalysisView = Backbone.View.extend({
+
+  initialize:function(){
+    var canvas = document.getElementById("analysis-canvas");
+    var ctx = this.ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.strokeRect(10,20,50,100);
+    ctx.beginPath();
+    ctx.moveTo(75,50);
+    ctx.lineTo(100,75);
+    ctx.lineTo(100,25);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(200,200,50,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.arc(200,200,25,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.closePath();
+  }
+});
--- proj/php/pages/analysis.php
+++ proj/php/pages/analysis.php
@@ -1,3 +1,5 @@
 <div id="analysis-view" class="clearfix">
-  analysis
+  <div>account information</div>
+  <div>month selection</div>
+  <canvas id="analysis-canvas" width=900 height="400" style="border:1px solid green"></canvas>
 </div>
